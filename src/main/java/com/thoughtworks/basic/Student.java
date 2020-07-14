d033bb26c931bdc42f19de06d9ac95f62e2eb503
yangjingsheng <673630886@qq.com>
2020-07-14 20:07:05 +0800

    should_return_introduce_when_introduce

---
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.basic;
+
+public class Student extends Person{
+    public Student(int age, String name, int classNum) {
+        super(age, name);
+    }
+    private int classNum;
+}
---
+++ src/test/java/com/thoughtworks/basic/StudentTest.java
@@ -0,0 +1,21 @@
+package com.thoughtworks.basic;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class StudentTest {
+    @Test
+    public void should_return_introduce_when_introduce() {
+        //given
+        Student student = new Student(21,"Tom",2);
+        //when
+        String result = student.introduce();
+        //then
+        assertEquals(result,"My name is Tom.I am 21 years old.I am a Student of Class 1.");
+}
+
+}
