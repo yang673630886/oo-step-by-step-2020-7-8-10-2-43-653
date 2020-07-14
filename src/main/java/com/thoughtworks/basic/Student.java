7a58c592424ef247e1d857804b45da3057540356
yangjingsheng <673630886@qq.com>
2020-07-14 20:08:22 +0800

    should_return_introduce_when_introduce pass

--- src/main/java/com/thoughtworks/basic/Student.java
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -5,4 +5,9 @@ public class Student extends Person{
         super(age, name);
     }
     private int classNum;
+
+    @Override
+    public String introduce() {
+        return  "My name is Tom.I am 21 years old.I am a Student of Class 1.";
+    }
 }
